996783dedc7ed9163d268350dec8ec6f916f3d76
ollvr <omarobouhawelo15@gmail.com>
2023-12-24 15:52:51 +0100

    Initial commit

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),  (req, res) => {
+  console.log(req.file);
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
